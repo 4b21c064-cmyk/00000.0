a988eb7b71636e40022ffba88bbb35ad9358b6b4
4b21c064-cmyk <4b21c064@stust.edu.tw>
2026-06-03 10:20:13 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'zenflight-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// 安裝並快取網頁資源
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 攔截請求，實現離線也能秒開 App
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
